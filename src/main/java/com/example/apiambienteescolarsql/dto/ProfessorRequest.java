72d6e7ff70692875ca9ca3ac6821897ae121e9b3
Lívia Izidório <liviaizidorio.li@gmail.com>
2026-03-18 23:08:48 -0300

    fix: corrigindo professor request

--- src/main/java/com/example/apiambienteescolarsql/dto/ProfessorRequest.java
+++ src/main/java/com/example/apiambienteescolarsql/dto/ProfessorRequest.java
@@ -1,7 +1,13 @@
 package com.example.apiambienteescolarsql.dto;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProfessorRequest {
     @NotNull
     private String nome;
